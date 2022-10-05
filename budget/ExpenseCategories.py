aca01ecf4f5904a4dcd17b360681bbd78c07e97f
Christopher Tant <tant@vir-aureum.local>
2022-10-05 09:51:04 -0400

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -15,8 +15,16 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
 
-    #timeit.timeit(stmt = "pass" setup = '''
-#''', number=100000, globals=globals())
+    print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
+    setup = 
+    '''
+    from . import Expense
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    ''', 
+    number=100000,
+    globals=globals()
+    ))
 
 
 
